82fbe39189d2c31c6058c15de1b771f1c9f9d575
nclarkson147 <77694148+nclarkson147@users.noreply.github.com>
2021-05-12 23:37:15 -0500

    Update radix-sort.py

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,10 +1,100 @@
 import urllib
 import requests
-
+import queue
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    booktxt = urllib.request.urlopen(book_url).read().decode()
-    bookascii = booktxt.encode('ascii','replace')
-    return bookascii.split()
+    btxt = urllib.request.urlopen(book_url).read().decode()
+    bascii = btxt.encode('ascii','replace')
+    return bascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    lst = radix_sort()
+    proper = sorted(book_to_words())
+    arr = []
+    for i in range(len(lst)):
+        arr.append(lst[i].decode("ascii"))
+    return arr
+
+def itemsToQueues(words,k,maxlen):
+    temp = [None] * 256
+    for i in range(0,256):
+        temp[i] = []
+    for j in range(0,len(words)):
+        pos = get_pos(words[j], k, maxlen)
+        temp[pos].append(words[j])
+    return temp
+
+def get_pos(word, k, max):
+    pos = max - k - 1
+    if pos >= len(word):
+        return 0
+    else:
+        return word[pos]
+
+def queuesToArray(queues,numWords):
+    words = [ [] for i in range(numWords) ]
+    ind = 0
+    for i in range(0, len(queues)):
+        cur = queues[i]
+        while len(cur) > 0:
+            words[ind] = cur.pop(0)
+            ind += 1
+    return words
+
+def radix_sort():
+    lw = calc_lenest_word()
+    words = book_to_words()
+    for i in range(0,lw):
+        words = queuesToArray(itemsToQueues(words,i,lw),len(words))
+    return words
+
+def calc_lenest_word():
+    max = 1
+    lst = book_to_words()
+    for i in lst:
+        if(len(i) > max):
+            max = len(i)
+    return max
+
+################################################################################
+# TEST CASES
+################################################################################
+
+def test_book():
+    arr = []
+    w = book_to_words()
+    for i in range(len(book_to_words())):
+        arr.append(w[i].decode("ascii"))
+    arr = sorted(arr)
+    lst = radix_a_book()
+    n = 0
+    for i in range(len(arr)):
+        if not arr[i]==lst[i]:
+            print("Python Sort")
+            print(arr[i])
+            print()
+            print("Radix Sort")
+            print(lst[i])
+            raise Exception
+        n+=1
+        print("Correct Comparisons = " + str(n))
+    return True
+
+
+def say_test(f):
+    print(80 * "#" + "\n" + f.__name__ + "\n" + 80 * "#" + "\n")
+
+def say_success():
+    print("----> SUCCESS")
+
+################################################################################
+# MAIN
+################################################################################
+def main():
+    for t in [test_book]:
+        say_test(t)
+        t()
+        say_success()
+    print(80 * "#" + "\nALL TEST CASES FINISHED SUCCESSFULLY!\n" + 80 * "#")
+
+if __name__ == '__main__':
+    main()
